b2b8c34abe45d90b15800b4ee9ce4d9c3d570d6a
ArijitKD <arijitkdgit.official@gmail.com>
2022-09-19 17:22:08 +0530

    Update build.py

--- build.py
+++ build.py
@@ -8,7 +8,7 @@ if (__name__ == "__main__"):
     KCOFILE = "kc.o"
     KCFILE = "kernel.c"
     KERNELBIN = "kernel.bin"
-    OSNAME = "Griffin"
+    OSNAME = "Griffin OS"
     OUTPUT_PATH = OSNAME+"/boot/"+KERNELBIN
     CWD = os.getcwd()
     QEMU_PKG = "qemu-system-i386"
