be7ca75331d21cd76c2f477a8ab48d9b02aa2743
Arijit Kumar Das <arijitkrdas2004@outlook.com>
2024-04-07 03:10:41 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
-clear && python3 build.py
-printf "Press Enter to exit..."
+nasm -f elf32 kernel.asm -o obj/kasm.o
+clang -m32 -c kernel.c -o obj/kc.o -ffreestanding
+clang -m32 -c drivers/vga.c -o obj/vga.o -ffreestanding
+clang -m32 -c drivers/kernellib/string.c -o obj/string.o -ffreestanding
+clang -m32 -c drivers/kernellib/math.c -o obj/math.o -ffreestanding
+ld -m elf_i386 -T link.ld -o kernel obj/kasm.o obj/kc.o obj/vga.o obj/string.o obj/math.o -z noexecstack
+qemu-system-i386 -kernel kernel
 read r
-echo
