2666b865e4c6f31ec5461e2c049e84b456398d74
Arijit Kumar Das <arijitkrdas2004@outlook.com>
2024-04-10 13:19:45 +0530

    Update math.c
    Fixed bug in roundint() function for rounding off negative numbers.

--- drivers/kernellib/math.c
+++ drivers/kernellib/math.c
@@ -2,8 +2,8 @@
 
 int roundint(float n)
 {
-  if ((n-(int)n)>=0.5)
-    return n+1;
-  else
-    return n;
+    if (n >= 0)
+        return ((n - (int)n) >= 0.5) ? (n + 1) : n;
+    else
+        return (((int)n - n) >= 0.5) ? (n - 1) : n;
 }
